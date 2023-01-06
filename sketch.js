99fb793fe4a1db7f2e3b5858974db52d5cfb0b0e
Mdfs05 <55096826+Mdfs05@users.noreply.github.com>
2023-01-05 23:20:49 -0500

    Update sketch.js: Rendering the view

--- sketch.js
+++ sketch.js
@@ -2,42 +2,79 @@ let walls = [];
 let ray;
 let particle;
 let xoff = 0;
-let yoff = 1000;
+let yoff = 10000;
+
+let sceneW = 400;
+let sceneH = 400;
+let sliderFOV;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 400);
   //Making a new boundary instance
   for(let i = 0; i < 5; i++) {
-    let x1 = random(width);
-    let x2= random(width);
-    let y1 = random(width);
-    let y2 = random(width);
+    let x1 = random(sceneW);
+    let x2= random(sceneW);
+    let y1 = random(sceneH);
+    let y2 = random(sceneH);
     walls[i] = new Boundary(x1, x2, y1, y2);
   }
-  walls.push(new Boundary(0, 0, width, 0));
-  walls.push(new Boundary(width, 0, width, height));
-  walls.push(new Boundary(width, height, 0, height));
-  walls.push(new Boundary(0, height, 0, 0));
+  walls.push(new Boundary(0, 0, sceneW, 0));
+  walls.push(new Boundary(sceneW, 0, sceneW, sceneH));
+  walls.push(new Boundary(sceneW, sceneH, 0, sceneH));
+  walls.push(new Boundary(0, sceneH, 0, 0));
   particle = new Particle();
+  sliderFOV = createSlider(0, 360, 45);
+  sliderFOV.input(changeFOV);
   //ray = new Ray(100, 200);
 }
 
+function changeFOV() {
+  const fov = sliderFOV.value();
+  particle.updateFOV(fov);
+}
+
 function draw() {
+  if (keyIsDown(RIGHT_ARROW)) {
+    particle.rotate(0.05);
+  } if (keyIsDown(LEFT_ARROW)) {
+    particle.rotate(-0.05);
+  } if (keyIsDown(UP_ARROW)) {
+    particle.move(1);
+  } if (keyIsDown(DOWN_ARROW)) {
+    particle.move(-1);
+  }
+  
   background(0);
-  fill(255, 0, 0);
-  textSize(35);
-  text("IT IS COMING", 100, 350);
+  //fill(255, 0, 0);
+  //textSize(35);
+  //text("IT IS COMING", 100, 350);
   //Telling the wall and ray to show
   for(let wall of walls) {
       wall.show();
   }
   //particle.update(mouseX, mouseY);
-  particle.update(noise(xoff)*width, noise(yoff)*width);
+  //particle.update(noise(xoff)*sceneW, noise(yoff)*sceneH);
   particle.show();
-  particle.look(walls);
+    
+  //xoff += 0.01;
+  //yoff += 0.01;
   
-  xoff += 0.01;
-  yoff += 0.01;
+  const scene = particle.look(walls);
+  const w = sceneW / scene.length;
+  push();
+  translate(sceneW, 0);
+  for(let i = 0; i < scene.length; i++) {
+    noStroke();
+    const sq = scene[i]*scene[i];
+    const wSq = sceneW * sceneW;
+    const b = map(sq, 0, wSq, 255, 0);
+    const h = map(scene[i], 0, sceneW, sceneH, 0);
+    fill(b);
+    rectMode(CENTER);
+    rect((i* (w + w))/2, sceneH/2, w+1, h);
+    //17:15
+  }
+  pop();
   // ray.show();
   // ray.lookAt(mouseX, mouseY);
   
