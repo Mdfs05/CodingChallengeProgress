461b1ec0420c19f2471a322b8f5facf624dfb96f
Mdfs05 <55096826+Mdfs05@users.noreply.github.com>
2023-01-05 23:21:55 -0500

    Update particle.js:  Rendering the view

--- particle.js
+++ particle.js
@@ -1,11 +1,36 @@
 class Particle {
   constructor() {
-    this.pos = createVector(width/2, height /2);
+    this.fov = 45;
+    this.pos = createVector(sceneW/2, sceneH/2);
     this.rays = [];
+    this.heading = 0;
     //Creating rays casting out in all diretions using position and the angle (raidians)
-    for(let a = 0; a < 360; a+=1) {
+    for(let a = (-this.fov) /2; a < this.fov/2; a+=1) {
       this.rays.push(new Ray(this.pos, radians(a)));
     }
+  }
+  
+  updateFOV(fov) {
+    this.fov = fov;
+    this.rays = [];
+    for(let a = (-this.fov) /2; a < this.fov/2; a+=1) {
+      this.rays.push(new Ray(this.pos, radians(a) + this.heading));
+    }
+  }
+  
+  rotate(angle) {
+    this.heading += angle;
+    let index = 0;
+    for(let a = (-this.fov) /2; a < this.fov/2; a += 1) {
+      this.rays[index].setAngle(radians(a) + this.heading);
+      index++;
+    }
+  }
+  
+  move(amt) {
+    const vel = p5.Vector.fromAngle(this.heading);
+    vel.setMag(amt);
+    this.pos.add(vel);
   }
   
   update(x,y) {
@@ -13,14 +38,20 @@ class Particle {
   }
   
   look(walls) {
-    for(let ray of this.rays) {
+    const scene = [];
+    for(let i = 0; i < this.rays.length; i++) {
+      const ray = this.rays[i];
       let closest = null;
       let record = Infinity;
       for(let wall of walls) {
         const pt = ray.cast(wall);
         if(pt) {
           //line(this.pos.x, this.pos.y, pt.x, pt.y);
-          const d = p5.Vector.dist(this.pos, pt);
+          let d = p5.Vector.dist(this.pos, pt);
+          const a = ray.dir.heading() - this.heading;
+          if(!mouseIsPressed) {
+            d *= cos(a);
+          }
           if(d < record) {
             record = d;
             closest = pt;
@@ -29,15 +60,17 @@ class Particle {
       }
       if(closest) {
         //replace rgb value with '255' to make rays white, leave last value of 100 for transparency
-        stroke(255, 0, 0, 100);
+        stroke(255, 100);
         line(this.pos.x, this.pos.y, closest.x, closest.y);
       }
+      scene[i] = record;
     }
+    return scene;
   }
   
   show() {
     //255 for a white gorb
-    fill(255, 0, 0);
+    fill(255);
     ellipse(this.pos.x, this.pos.y, 16);
     for (let ray of this.rays) {
       ray.show();
